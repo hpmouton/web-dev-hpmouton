3ba3d13be427b5687db7dffa9eb61d046e1ecb3c
hpmouton <hmouton786@gmail.com>
2025-06-06 03:09:34 +0200

    feat: added api docs and link on home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,14 +29,9 @@
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                             <a href="/" class="rounded-md bg-yellow-900 px-3 py-2 text-sm font-medium text-white"
                                 aria-current="page">Rate Checker</a>
-                            <a href="#"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-yellow-900 hover:bg-yellow-700   hover:text-white">Team</a>
-                            <a href="#"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-yellow-900 hover:bg-yellow-700   hover:text-white">Projects</a>
-                            <a href="#"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-yellow-900  hover:bg-yellow-700   hover:text-white">Calendar</a>
-                            <a href="#"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-yellow-900  hover:bg-yellow-700  hover:text-white">Reports</a>
+                            <a href="/docs"
+                                class="rounded-md px-3 py-2 text-sm font-medium text-yellow-900 hover:bg-yellow-700   hover:text-white">API Docs</a>
+
                         </div>
                     </div>
                 </div>
